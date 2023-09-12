094d918f6b1c3c8b91821d9ef8ae7aa73044ccee
marekth <marek@thespian.ca>
2023-09-11 17:10:22 -0700

    nouveau changement

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -11,7 +11,7 @@ from struct import * # Import struct to use unpack
 from tf.transformations import euler_from_quaternion # Import to transform quaternion to yaw
 
 
-positionFormat = "fffI" # Format is three 32-bit floats for position of vehicule and one 32-bit integer for vehicule ID
+vehiculeFormat = "fffI" # Format is three 32-bit floats for position of vehicule and one 32-bit integer for vehicule ID
 positionFormat = "fffxxxx" # Format is three 32-bit floats and four empty bytes for position of vehicule
 obstacleFormat = "Ixxxxxxxxxxxx" # Format is one 32-bit integer and twelve empty bytes
 idFormat = "Ixxxxxxxxxxxx" # Format is one 32-bit integer and twelve empty bytes
@@ -46,7 +46,7 @@ class ROSMonitor:
 
         self.racecarSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Socket uses IPv4 and is a Datagram socket (Message destination unknown; broadcast to all potential listeners)
         self.racecarSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Enable broadcasting on the socket for sending data to multiple devices on the same network segment
-        self.racecarSocket.bind(('10.0.1.19',self.pos_broadcast_port)) # Bind to address
+        self.racecarSocket.bind(('10.0.1.0/24',self.pos_broadcast_port)) # Bind to address
         
         self.broadcastFrequency = rospy.Timer(rospy.Duration(1), self.broadcastFrequency_cb) # Broadcast vehicle position at 1 Hz frequency
  
@@ -61,7 +61,7 @@ class ROSMonitor:
             if (distance < 1) and (distance >= data.range_min) and (distance <= data.range_max): # If distance of obstacle shorter than 1m
                 self.obstacle = True # Obstacle detected
     def broadcastFrequency_cb(self, event):
-        vehiculePosPack = pack(positionFormat,self.pos[0],self.pos[1],self.pos[2],self.id) # Convert string to binary
+        vehiculePosPack = pack(vehiculeFormat,self.pos[0],self.pos[1],self.pos[2],self.id) # Convert string to binary
         self.racecarSocket.sendto(vehiculePosPack,("10.0.1.255",self.pos_broadcast_port)) # Send vehicule position NEED TO FIND ADDRESS!
 
     def rr_loop(self):
--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -7,13 +7,13 @@ HOST = '127.0.0.1'
 # This process should listen to a different port than the RemoteRequest client.
 PORT = 65431
 
-positionFormat = "fffI" # Format is three 32-bit floats for position of vehicule and one 32-bit integer for vehicule ID
+vehiculeFormat = "fffI" # Format is three 32-bit floats for position of vehicule and one 32-bit integer for vehicule ID
 
 racecarSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Socket uses IPv4 and is a Datagram socket (Message destination unknown; broadcast to all potential listeners)
 racecarSocket.bind((HOST, PORT)) # Bind the socket to the  host and port
 
 while True:
 	(vehiculePos, vehiculeId) = racecarSocket.recvfrom(16) # Receives vehicule position and vehicule ID from the socket (Datagram)
-	vehiculePosUnpack = unpack(positionFormat,vehiculePos) # Convert binary to string
+	vehiculePosUnpack = unpack(vehiculeFormat,vehiculePos) # Convert binary to string
 	print("ID du vehicule : ",vehiculeId) # Print vehicule ID
 	print("Position du vehicule : ",vehiculePosUnpack) # Print vehicule position
