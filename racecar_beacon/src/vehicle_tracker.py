6d5bb3b581fb29b4b7c940601e4d13dd4f838498
marekth <marek@thespian.ca>
2023-09-13 18:28:16 -0700

    change bind for connect

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -16,7 +16,7 @@ PORT = 65431
 vehiculeFormat = endianness_char + "fffI" # Format is three 32-bit floats for position of vehicule and one 32-bit integer for vehicule ID
 
 racecarSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Socket uses IPv4 and is a Datagram socket (Message destination unknown; broadcast to all potential listeners)
-racecarSocket.bind((HOST, PORT)) # Bind the socket to the  host and port
+racecarSocket.connect((HOST, PORT)) # Connect the socket to the  host and port
 
 while True:
 	(vehiculePos, connectionId) = racecarSocket.recvfrom(16) # Receives vehicule position and vehicule ID from the socket (Datagram)
